97690d8219b71965ef472d5a7fd488de2d4130ae
shadik <shadikmahamudsohan@gmail.com>
2022-06-24 22:30:44 +0600

    setting up starting server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+//middleware
+app.use(cors());
+app.use(express.json());
