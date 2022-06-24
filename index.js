eca6ed652732d7f61312714b532f7d8e71abcad3
shadik <shadikmahamudsohan@gmail.com>
2022-06-24 22:44:21 +0600

    connecting to mongodb

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9p5xs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('connected');
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
