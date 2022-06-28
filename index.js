4724b1a4982dcdb167c8e1b72b311f39968c1aee
shadik <shadikmahamudsohan@gmail.com>
2022-06-29 00:44:02 +0600

    adding server to heroku

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 
+//middleware
+app.use(cors());
+app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9p5xs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('connected');
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('connected to mongodb');
+
+        const houseCollection = client.db("houses").collection("house");
+
+        // get all data from database
+        app.get('/', async (req, res) => {
+            const housers = await houseCollection.find({}).toArray();
+            res.send(housers);
+        });
+        // get all data from database
+        app.get('/houses', async (req, res) => {
+            const housers = await houseCollection.find({}).toArray();
+            res.send(housers);
+        });
+
+        // get single data from database
+        app.get('/houses/:id', async (req, res) => {
+            const id = req.params.id;
+            const find = { _id: ObjectId(id) };
+            const singleHouse = await houseCollection.findOne(find);
+            res.send(singleHouse);
+        });
+
+        // filter house
+        app.get('/filter-houses', async (req, res) => {
+            const data = req.body;
+            const filtered = await houseCollection.find(data).toArray;
+            res.send(filtered);
+        });
 
 
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
